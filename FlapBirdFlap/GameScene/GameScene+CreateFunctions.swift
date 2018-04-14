e44f3fc79a49a499c188f133950e0e855628323d
Goran Pavlovic <pavlovicgoran94@gmail.com>
2018-04-14 12:03:00 +0200

    GameScene refactored in 2 files

---
+++ FlapBirdFlap/GameScene/GameScene+CreateFunctions.swift
@@ -0,0 +1,74 @@
+//
+//  GameScene+CreateFunctions.swift
+//  FlapBirdFlap
+//
+//  Created by Goran Pavlovic on 4/14/18.
+//  Copyright © 2018 Goran Pavlovic. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SpriteKit
+
+extension GameScene{
+    
+    func createPipes(){
+        setRightXPipe()
+        createLeftPipes()
+        createRightPipes()
+    }
+    
+    private func setRightXPipe(){
+        pipeRightX = frame.width + 200
+    }
+    
+    private func createLeftPipes(){
+        for i in 0 ..< numberOfPipes{
+            createPipe(rotation: -CGFloat.pi/2, position: CGPoint(x: pipeLeftX, y: frame.height - heightOffset - CGFloat(i)*pipeSpace), column: Column.left)
+        }
+    }
+    
+    private func createRightPipes(){
+        for i in 0 ..< numberOfPipes{
+            createPipe(rotation: CGFloat.pi/2, position: CGPoint(x: pipeRightX, y: frame.height - heightOffset - CGFloat(i)*pipeSpace), column: Column.right)
+        }
+    }
+    
+    private func createPipe(rotation: CGFloat, position: CGPoint, column: Column){
+        let pipe = Pipe(rotation: rotation, position: position, scale: scale)
+        pipe.setColumn(column: column)
+        addChild(pipe)
+        array.append(pipe)
+    }
+    
+    func createBackground(){
+        let position = CGPoint(x: frame.width/2, y: frame.height/2)
+        let background = Background(position: position, scale: scale)
+        addChild(background)
+    }
+    
+    func createScore() {
+        scoreLabel = SKLabelNode(fontNamed: "Optima-ExtraBlack")
+        scoreLabel.fontSize = 24
+        
+        scoreLabel.position = CGPoint(x: frame.midX, y: frame.maxY - 60)
+        scoreLabel.text = "SCORE: 0"
+        scoreLabel.fontColor = UIColor.white
+        scoreLabel.zPosition = 7
+        
+        addChild(scoreLabel)
+    }
+    
+    func createBase(){
+        let position = CGPoint(x: frame.width/2, y: 0)
+        let base = Base(position: position, scale: scale)
+        addChild(base)
+    }
+    
+    func createBird(){
+        
+        let startPosition = CGPoint(x: frame.width/2, y: frame.height/2)
+        bird = Bird(scale: scale, position: startPosition)
+        addChild(bird)
+    }
+}
--- FlapBirdFlap/GameScene/GameScene.swift
+++ FlapBirdFlap/GameScene/GameScene.swift
@@ -103,65 +103,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
 
     }
     
-    private func createPipes(){
-        setRightXPipe()
-        createLeftPipes()
-        createRightPipes()
-    }
-    
-    private func setRightXPipe(){
-        pipeRightX = frame.width + 200
-    }
-    
-    private func createLeftPipes(){
-        for i in 0 ..< numberOfPipes{
-            createPipe(rotation: -CGFloat.pi/2, position: CGPoint(x: pipeLeftX, y: frame.height - heightOffset - CGFloat(i)*pipeSpace), column: Column.left)
-        }
-    }
-    
-    private func createRightPipes(){
-        for i in 0 ..< numberOfPipes{
-            createPipe(rotation: CGFloat.pi/2, position: CGPoint(x: pipeRightX, y: frame.height - heightOffset - CGFloat(i)*pipeSpace), column: Column.right)
-        }
-    }
-    
-    private func createPipe(rotation: CGFloat, position: CGPoint, column: Column){
-        let pipe = Pipe(rotation: rotation, position: position, scale: scale)
-        pipe.setColumn(column: column)
-        addChild(pipe)
-        array.append(pipe)
-    }
-    
-    private func createBackground(){
-        let position = CGPoint(x: frame.width/2, y: frame.height/2)
-        let background = Background(position: position, scale: scale)
-        addChild(background)
-    }
-    
-    func createScore() {
-        scoreLabel = SKLabelNode(fontNamed: "Optima-ExtraBlack")
-        scoreLabel.fontSize = 24
-        
-        scoreLabel.position = CGPoint(x: frame.midX, y: frame.maxY - 60)
-        scoreLabel.text = "SCORE: 0"
-        scoreLabel.fontColor = UIColor.white
-        scoreLabel.zPosition = 7
-        
-        addChild(scoreLabel)
-    }
-    
-    private func createBase(){
-        let position = CGPoint(x: frame.width/2, y: 0)
-        let base = Base(position: position, scale: scale)
-        addChild(base)
-    }
-    
-    private func createBird(){
-        
-        let startPosition = CGPoint(x: frame.width/2, y: frame.height/2)
-        bird = Bird(scale: scale, position: startPosition)
-        addChild(bird)
-    }
+
     
     
 }
